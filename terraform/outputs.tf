a22547083813b49b174bb13d79e37674a1a0820f
HarveyAland <harvey.aland.99@outlook.com>
2025-06-05 18:54:44 +0100

    Restore Terraform code and add Jenkinsfile, Dockerfile, screenshots

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,13 +2,13 @@ output "vpc_id" {
   value = module.vpc.vpc_id
 }
 
-output "public_subnets" {
-  value = module.vpc.public_subnets
-}
+#utput "public_subnets" {
+  #value = module.vpc.public_subnets
+#}
 
-output "private_subnets" {
-  value = module.vpc.private_subnets
-}
+#output "private_subnets" {
+  #value = module.vpc.private_subnets
+#}
 
 output "eks_cluster_id" {
   value = module.eks.eks_cluster_id
